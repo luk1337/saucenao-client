e06ee9a5220baed3c02551d4d85547c6fbac2fdf
LuK1337 <priv.luk@gmail.com>
2018-09-14 22:27:14 +0200

    use createStackNavigator instead of StackNavigator

--- src/AppNavigation.js
+++ src/AppNavigation.js
@@ -1,9 +1,9 @@
-import { StackNavigator } from "react-navigation";
+import { createStackNavigator } from "react-navigation";
 
 import HomeScreen from "./screens/HomeScreen";
 import ResultScreen from "./screens/ResultScreen";
 
-const AppNavigation = StackNavigator({
+const AppNavigation = createStackNavigator({
   Home: { screen: HomeScreen },
   Result: { screen: ResultScreen },
 });
